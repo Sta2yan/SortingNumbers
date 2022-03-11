00432610ff383f7725105744e8c4290ea8f01e95
Sta2yan <s2_stas@mail.ru>
2022-03-11 15:17:56 +0300

    Добавьте файлы проекта.

---
+++ SortingNumbers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SortingNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 10, 9, 3, 4, 5, 6, 7, 8, 2, 1};
+
+            Console.WriteLine("До:");
+
+            foreach (int number in numbers)
+            {
+                Console.Write(number + "|");
+            }
+
+            Console.Write("\nПосле:");
+            Array.Sort(numbers);
+            Console.WriteLine();
+
+            foreach (int number in numbers)
+            {
+                Console.Write(number + "|");
+            }
+        }
+    }
+}
